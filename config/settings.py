a3f85150bd4c379fce3443bb47f9f1a6d1e1c305
Marc Johnson <marcjohnson518@gmail.com>
2020-10-30 09:41:37 -0400

    db updates

--- config/settings.py
+++ config/settings.py
@@ -105,16 +105,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'postgres',
-       'USER': 'postgres',
-       'PASSWORD': 'postgres',
-       'HOST': 'db',
-       'PORT': 5432
-   }
-}
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': 'db',
+#        'PORT': 5432
+#    }
+# }
 
 # 'default': {
 # 'ENGINE': 'django.db.backends.postgresql',
@@ -126,11 +126,11 @@ DATABASES = {
 # }
 
 
-# DATABASES = {
-#
-#         "default": env.dj_db_url("DATABASE_URL",
-#      default="postgres://postgres@db/postgres")
-#  }
+DATABASES = {
+
+        "default": env.dj_db_url("DATABASE_URL",
+     default="postgres://postgres@db/postgres")
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
